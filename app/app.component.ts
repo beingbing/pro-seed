6d585db5406b146f3f344167aa06ef9a3aea72c1
Samar Shaikh <samar@walkover.in>
2018-06-04 12:50:37 +0530

    samar | using ng-content with projection slots

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,16 +9,18 @@ import { User } from './auth-form/auth-form.interface';
       <auth-form 
         (submitted)="createUser($event)">
         <h3>Create account</h3>
+        <button type="submit">Join Us</button>
       </auth-form>
       <auth-form 
         (submitted)="loginUser($event)">
         <h3>Login</h3>
+        <button type="submit">Login</button>
       </auth-form>
     </div>
   `
 })
 export class AppComponent {
-  
+
   createUser(user: User) {
     console.log('Create account', user);
   }
