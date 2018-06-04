1b97a59c9bc53f34be9b36e1ede1dc4d6610fe4f
Samar Shaikh <samar@walkover.in>
2018-06-04 12:37:49 +0530

    samar | content projection

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AuthFormModule } from './auth-form/auth-form.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthFormModule
   ],
   bootstrap: [
     AppComponent
